59bf5cf747672e51eaa0783f591d3d41b2074d6e
PY Delens <pydelens@gmail.com>
2021-02-23 21:37:28 +0100

    strapi.io-getting-started-gatsby

---
+++ cms-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '04af6f439bd20c445fbbf483c7032d3f'),
+    },
+  },
+});
---
+++ cms-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a07a9a72-3e40-47f3-817e-cc16eaf0e270'
+};
